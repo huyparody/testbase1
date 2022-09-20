d40f9a984f33e200ba7fafdcc8d5099bb228370f
Trinh Duc Huy <huytd@educa.vn>
2022-09-20 17:04:29 +0700

    update

--- Package.swift
+++ Package.swift
@@ -24,7 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "educa-ios-base",
-            dependencies: []),
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+            ]),
         .testTarget(
             name: "educa-ios-baseTests",
             dependencies: ["educa-ios-base"]),
